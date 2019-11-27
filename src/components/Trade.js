6a4aa689b2e7b417b061341e941c3134c8900118
Mao <maoyuan@gmail.com>
2019-11-26 19:01:47 -0500

    submitButtonDisabled logic

--- src/components/Trade.js
+++ src/components/Trade.js
@@ -1,7 +1,6 @@
 import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import axios from 'axios';
-// import Popup from 'reactjs-popup';
 import BootstrapTable from 'react-bootstrap-table-next';
 
 
@@ -15,7 +14,7 @@ class Trade extends Component {
             orderPrice: '',
             orderVolumn: '',
             existingOrders: [],
-            orderPriceDisabled : false,
+            orderPriceDisabled: false,
             submitButtonDisabled: true
         };
     }
@@ -29,11 +28,11 @@ class Trade extends Component {
         this.setState({
             orderType: e.target.value
         });
-        if(e.target.value.includes('Market')){
+        if (e.target.value.includes('Market')) {
             this.setState({
                 orderPriceDisabled: true
             });
-        }else {
+        } else {
             this.setState({
                 orderPriceDisabled: false
             });
@@ -48,7 +47,7 @@ class Trade extends Component {
         this.setState({
             orderVolumn: e.target.value
         });
-        if(this.state.ticker != null && this.state.orderType != null && this.state.orderVolumn != null){
+        if (this.state.ticker.length !== 0 && this.state.orderType.length !== 0 && this.state.orderVolumn != null) {
             this.setState({
                 submitButtonDisabled: false
             });
@@ -75,7 +74,14 @@ class Trade extends Component {
             // 'http://localhost:8080/api/setorder/'
             'https://alphaspring.herokuapp.com/api/setorder/'
             , feedbackData).then(this.fetchTable);
-        this.setState({ticker: '', companyName: '', orderType: '', orderPrice: '', orderVolumn: '', submitButtonDisabled: true})
+        this.setState({
+            ticker: '',
+            companyName: '',
+            orderType: '',
+            orderPrice: '',
+            orderVolumn: '',
+            submitButtonDisabled: true
+        })
     };
 
     fetchTable = () => {
@@ -89,7 +95,7 @@ class Trade extends Component {
                     existingOrders: data.orders
                 })
             });
-    }
+    };
 
     componentDidMount() {
         this.fetchTable();
@@ -114,57 +120,61 @@ class Trade extends Component {
 
         return (
             <div className="container mx-auto">
-                    <hr/>
-                    <h3 className="display-5">Place Order</h3>
-                    <hr/>
-                    <form onSubmit={this.onSubmitHandler}>
-                        <div className="form-row">
-                            <div className="form-group col-md-6">
-                                <label className="input-group-text" htmlFor="inputGroupSelect01">Stock Name</label>
-                                <select className="custom-select" id="inputGroupSelect01" value={this.state.ticker} onChange={this.onChangeTickerHandler}>
-                                    <option value ="">Choose...</option>
-                                    <option value="ABN AMRO BANK N.V. (ABN)">ABN AMRO BANK N.V. (ABN)</option>
-                                    <option value="ADYEN (ADYEN)">ADYEN (ADYEN)</option>
-                                    <option value="ING GROEP N.V. (INGA)">ING GROEP N.V. (INGA)</option>
-                                    <option value="KPN KON (KPN)">KPN KON (KPN)</option>
-                                    <option value="ROYAL DUTCH SHELLA (RDSA)">ROYAL DUTCH SHELLA (RDSA)</option>
-                                    <option value="BNP PARIBAS ACT.A (BNP)">BNP PARIBAS ACT.A (BNP)</option>
-                                </select>
+                <hr/>
+                <h3 className="display-5">Place Order</h3>
+                <hr/>
+                <form onSubmit={this.onSubmitHandler}>
+                    <div className="form-row">
+                        <div className="form-group col-md-6">
+                            <label className="input-group-text" htmlFor="inputGroupSelect01">Stock Name</label>
+                            <select className="custom-select" id="inputGroupSelect01" value={this.state.ticker}
+                                    onChange={this.onChangeTickerHandler}>
+                                <option value="">Choose...</option>
+                                <option value="ABN AMRO BANK N.V. (ABN)">ABN AMRO BANK N.V. (ABN)</option>
+                                <option value="ADYEN (ADYEN)">ADYEN (ADYEN)</option>
+                                <option value="ING GROEP N.V. (INGA)">ING GROEP N.V. (INGA)</option>
+                                <option value="KPN KON (KPN)">KPN KON (KPN)</option>
+                                <option value="ROYAL DUTCH SHELLA (RDSA)">ROYAL DUTCH SHELLA (RDSA)</option>
+                                <option value="BNP PARIBAS ACT.A (BNP)">BNP PARIBAS ACT.A (BNP)</option>
+                            </select>
 
-                            </div>
-                            <div className="form-group col-md-6">
-                                <label className="input-group-text" htmlFor="inputGroupSelect01">Order Type</label>
-                                <select className="custom-select" id="inputGroupSelect01" value={this.state.orderType} onChange={this.onChangeOrderTypeHandler}>
-                                    <option value ="">Choose...</option>
-                                    <option value="Limited Buy">Limited Buy</option>
-                                    <option value="Limited Sell">Limited Sell</option>
-                                    <option value="Market Buy">Market Buy</option>
-                                    <option value="Market Sell">Market Sell</option>
-                                </select>
-
-                            </div>
-                            <div className="form-group col-md-6">
-                                <label className="input-group-text" htmlFor="inputGroupSelect01" >Order Price</label>
-                                <input type="number" className="form-control" name="orderPrice"
-                                       value={this.state.orderPrice} disabled = {this.state.orderPriceDisabled ? true : null}
-                                       onChange={this.onChangeOrderPriceHandler}
-                                       placeholder="Enter"/>
-                            </div>
-
-                            <div className="form-group col-md-6">
-                                <label className="input-group-text" htmlFor="inputGroupSelect01">Order Volumn</label>
-                                <input type="number" className="form-control" name="orderVolumn"
-                                       value={this.state.orderVolumn}
-                                       onChange={this.onChangeOrderVolumnHandler}
-                                       placeholder="Enter"/>
-                            </div>
                         </div>
-                        <button disabled={this.state.submitButtonDisabled} type="submit" id="subBtn" className="btn btn-success"  onClick={this.onSubmitHandler}>Submit Order</button>
-                        <hr/>
-                        <BootstrapTable bootstrap4={true} classes="table-striped table-dark" keyField='id'
-                                        data={existingOrders}
-                                        columns={columns} defaultSorted={defaultSorted}/>
-                    </form>
+                        <div className="form-group col-md-6">
+                            <label className="input-group-text" htmlFor="inputGroupSelect01">Order Type</label>
+                            <select className="custom-select" id="inputGroupSelect01" value={this.state.orderType}
+                                    onChange={this.onChangeOrderTypeHandler}>
+                                <option value="">Choose...</option>
+                                <option value="Limited Buy">Limited Buy</option>
+                                <option value="Limited Sell">Limited Sell</option>
+                                <option value="Market Buy">Market Buy</option>
+                                <option value="Market Sell">Market Sell</option>
+                            </select>
+
+                        </div>
+                        <div className="form-group col-md-6">
+                            <label className="input-group-text" htmlFor="inputGroupSelect01">Order Price</label>
+                            <input type="number" className="form-control" name="orderPrice"
+                                   value={this.state.orderPrice} disabled={this.state.orderPriceDisabled ? true : null}
+                                   onChange={this.onChangeOrderPriceHandler}
+                                   placeholder="Enter"/>
+                        </div>
+
+                        <div className="form-group col-md-6">
+                            <label className="input-group-text" htmlFor="inputGroupSelect01">Order Volumn</label>
+                            <input type="number" className="form-control" name="orderVolumn"
+                                   value={this.state.orderVolumn}
+                                   onChange={this.onChangeOrderVolumnHandler}
+                                   placeholder="Enter"/>
+                        </div>
+                    </div>
+                    <button disabled={this.state.submitButtonDisabled} type="submit" id="subBtn"
+                            className="btn btn-success" onClick={this.onSubmitHandler}>Submit Order
+                    </button>
+                    <hr/>
+                    <BootstrapTable bootstrap4={true} classes="table-striped table-dark" keyField='id'
+                                    data={existingOrders}
+                                    columns={columns} defaultSorted={defaultSorted}/>
+                </form>
             </div>
         )
     }
